bcd0618fb0cc7655aa89640f3011d9198bf2e62c
An-dy1 <andrea_sue2@icloud.com>
2022-06-14 14:10:50 -0500

    playing with managing the state of two buttons at once

--- App.tsx
+++ App.tsx
@@ -6,25 +6,38 @@ import Display from './Display';
 
 export default function App() {
   const [counter, setCounter] = React.useState(0);
-  const incrementCounter = () => setCounter(counter + 1);
+
+  const [counterOne, setCounterOne] = React.useState(0);
+  const incrementCounterOne = () => setCounterOne(counterOne + 1);
+
+  const [counterTwo, setCounterTwo] = React.useState(0);
+  const incrementCounterTwo = () => setCounterTwo(counterTwo + 2);
 
   // passing props as an object vs inline in the component (below)
-  const displayProps = {
-    message: counter,
+  const displayPropsOne = {
+    message: counterOne,
   };
 
-  const buttonProps = {
-    onClickHandler: incrementCounter,
+  const buttonByOneProps = {
+    incrementAmount: 1,
+    onClickHandler: incrementCounterOne,
+  };
+
+  const buttonByTwoProps = {
+    incrementAmount: 2,
+    onClickHandler: incrementCounterTwo,
   };
 
   return (
     <div>
       <h1>My Counter App</h1>
-      <Display message={counter} />
-      <Button clickHandler={incrementCounter} />
+      <Display message={counterOne} />
+      <Button {...buttonByOneProps} />
 
+      <Display message={counterTwo} />
+      <Button {...buttonByTwoProps} />
       {/* <Display {...displayProps} />
-      <Button {...buttonProps} /> */}
+       */}
     </div>
   );
 }
--- Button.tsx
+++ Button.tsx
@@ -8,5 +8,9 @@ import './style.css';
 // const [currentStateValue, functionToSetNewValue] = useState(initialState)
 
 export default function Button(props) {
-  return <button onClick={props.clickHandler}>+1</button>;
+  return (
+    <button onClick={() => props.onClickHandler()}>
+      +{props.incrementAmount}
+    </button>
+  );
 }
